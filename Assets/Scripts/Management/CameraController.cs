84af090e8dd30f6a260bf6c983bbb272745493fe
Reipoh Gabriel Baldos Arradaza <reipohgabriel13@gmail.com>
2025-03-12 08:13:14 +0800

    Initialize project structure with core files
    This commit sets up the initial project structure for Rekindle with its key features

---
+++ Assets/Scripts/Management/CameraController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Cinemachine;
+using Unity.Cinemachine;
+
+public class CameraController : Singleton<CameraController>
+{
+    private CinemachineCamera cinemachineVirtualCamera;
+
+    public void SetPlayerCameraFollow()
+    {
+        cinemachineVirtualCamera = Object.FindFirstObjectByType<CinemachineCamera>();
+        cinemachineVirtualCamera.Follow = PlayerController.Instance.transform;
+    }
+}
