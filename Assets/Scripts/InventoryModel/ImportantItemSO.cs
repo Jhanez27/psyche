322831ff91f381b432ac91dd82a7207a1a938d62
Steve Francis King Pada <22-1-01322@gmail.com>
2025-04-16 23:19:42 +0800

    Added ImportantItemSO for nondroppable and essential items

--- Assets/Scripts/InventoryModel/ImportantItemSO.cs
+++ Assets/Scripts/InventoryModel/ImportantItemSO.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Inventory.Model
@@ -5,13 +7,18 @@ namespace Inventory.Model
     [CreateAssetMenu(fileName = "ImportantItemSO", menuName = "Scriptable Objects/ImportantItemSO")]
     public class ImportantItemSO : ItemSO, IItemAction
     {
+        [SerializeField]
+        List<Action> usages = new List<Action>();
         public string ActionName => "Use";
         [field: SerializeField]
         public AudioClip ActionSFX { get; private set; }
         public bool PerformAction(GameObject character)
         {
-            // Implement the action logic for important items here
-            Debug.Log($"Using important item: {name}");
+            foreach (Action action in usages)
+            {
+                action?.Invoke();
+            }
+
             return true;
         }
 
