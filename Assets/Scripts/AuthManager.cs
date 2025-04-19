af83285a6af58f8270611436f7ac62eaeaaee23b
Jhanez27 <rigensuringa2@gmail.com>
2025-04-19 15:38:58 +0800

    Added scripting to Unity
    Integrated the script to the buttons

--- Assets/Scripts/AuthManager.cs
+++ Assets/Scripts/AuthManager.cs
@@ -16,6 +16,9 @@ public class AuthManager : MonoBehaviour
     public TMP_InputField emailInput;
     public TMP_InputField passwordInput;
     public TMP_Text responseText;
+    public GameObject responseBox;
+    public GameObject accountManage;
+    public GameObject main_menu;
 
     private string supabaseUrl = "https://vrebhfnaijcupcyrcjiw.supabase.co";
     private string supabaseApiKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZyZWJoZm5haWpjdXBjeXJjaml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4NjE3ODAsImV4cCI6MjA1ODQzNzc4MH0.Fe2GEGppHlBfdqNaQJtShw6P7LjU5dfQp-hM6q8UM_U";
@@ -46,14 +49,19 @@ public class AuthManager : MonoBehaviour
         if (request.result == UnityWebRequest.Result.Success)
         {
             string response = request.downloadHandler.text;
-            responseText.text = "✅ Login successful!";
+            responseBox.SetActive(true);
+            emailInput.text = "";
+            passwordInput.text = "";
             Debug.Log("Login success: " + response);
+            accountManage.SetActive(false);
+            main_menu.SetActive(false);
         }
         else
         {
             string raw = request.downloadHandler.text;
             SupabaseError error = JsonUtility.FromJson<SupabaseError>(raw);
-            responseText.text = (error?.msg ?? "Login failed. Please try again.");
+            responseBox.SetActive(true);
+            responseText.text = "" + (error?.msg ?? "Login failed. Please try again.");
         }
     }
 }
